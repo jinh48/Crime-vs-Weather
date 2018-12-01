b60b0c54d8ea517afc2eea02dc4ca791cfe89258
Nikolai Liang <nikolaihegeman@gmail.com>
2018-12-01 13:20:28 -0800

    merging two data sets to create crime data, added the neighborhood map to data folder

--- process_data.R
+++ process_data.R
@@ -1,4 +1,7 @@
+library(dplyr)
 library("RSocrata")
+
+# ---- WEATHER ----
 source("keys.R")
 
 df <- read.socrata(
@@ -6,3 +9,21 @@ df <- read.socrata(
   app_token = "LohlCfXXXfYI7xjWHo8PBNHhr",
   stringsAsFactors = FALSE
 )
+
+
+# ---- CRIME DATA ----
+crime_data <- as.data.frame(read.csv("data/Crime_Data.csv"))
+
+my_neigh_map <- as.data.frame(read.csv("data/My_Neighborhood_Map.csv"))
+
+# capitalizing column Common.Name
+my_neigh_map[,2] = toupper(my_neigh_map[,2])
+
+# changing Common.Name to Neighborhood so that it will merge properly 
+neigh_data <- my_neigh_map %>% rename(Neighborhood = Common.Name)
+
+# mergign neighborhood data to crime data to get longitude and latitude
+combined <- merge(x = crime_data, y = neigh_data, by = "Neighborhood", all.x = TRUE)
+
+# filter out null values
+crime <- combined %>% filter(!is.na(Longitude))

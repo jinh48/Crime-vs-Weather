44993387c9c4b1ffeb9807c58ae074c9b53f6e53
Jin Ning Huang <jinh48@uw.edu>
2018-12-01 17:24:54 -0800

    add a ggmap code

---
+++ server.R
@@ -0,0 +1,39 @@
+library(shiny)
+library(plotly)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(ggmap)
+library(data.table)
+library(ggrepel)
+# library(rworldmap)
+# library(rworldxtra)
+library(maps)
+library(mapdata)
+
+source("process_data.R")
+#View(crime)
+
+states <- map_data("state")
+washington <- subset(states, region == "washington")
+counties <- map_data("county")
+wa_county <- subset(counties, region == "washington")
+
+
+
+washington_base <- ggplot(data = washington, mapping = aes(x = long, y = lat, group = group)) +
+  geom_polygon(fill = "palegreen", color = "black") +
+  coord_fixed(xlim = c(-123, -121), ylim = c(47,48), ratio = 1.3) +
+  theme_nothing() +
+  geom_polygon(data = wa_county, fill = NA, color = "white") +
+  geom_polygon(color = "black", fill = NA) + 
+  geom_point(data = crime, mapping = aes(x = crime$Longitude, y = crime$Latitude), 
+             color = "red", inherit.aes = FALSE)
+
+
+server <- function(input, output) {
+  output$mapPlot <- renderPlot(washington_base)
+}
+
+
+shinyServer(server)
--- ui.R
+++ ui.R
@@ -1,9 +1,20 @@
 library(shiny)
-library(plotly)
-library(dplyr)
+library(rsconnect)
 
 ui <- fluidPage(
-  
+  titlePanel("Crime vs Weather"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("date", "Date:",
+                  min = 0, max = 1000,
+                  value = 0),
+      sliderInput("zoom", "Zoom In/Out (%):",
+                  min = 0, max = 100,
+                  value = 100)),
+    mainPanel(
+      plotOutput("mapPlot")
+    )
+  )
 )
 
 shinyUI(ui)

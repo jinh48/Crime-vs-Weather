9b1e20880f76b7131737a90c6c94ccd71028ee46
Nikolai Liang <nikolaihegeman@gmail.com>
2018-12-01 17:52:52 -0800

    Rachel and I processed the weather data to select, filter, separate it into what we needed

--- process_data.R
+++ process_data.R
@@ -1,13 +1,26 @@
-library(dplyr)
-library("RSocrata")
-
+library("dplyr")
+library("tidyr")
 
 
 # ---- WEATHER ----
-source("keys.R")
 
-# ---- WEATHER DATA ----
-
+# weather_data <- data.table::fread("data/Road_Weather_Information_Stations.csv", header = TRUE, stringsAsFactors = FALSE)
+# 
+# select_loc_air <- select(weather_data, StationLocation, AirTemperature, DateTime)
+# 
+# weather_selected <- separate(select_loc_air, StationLocation, c("Latitude", "Longitude"), sep = ",")
+# 
+# weather_date_split <- separate(weather_selected, DateTime, c("Date", "Time"), sep = " ", convert = TRUE, as.is = TRUE)
+# 
+# weather_date_split[] <- lapply(weather_date_split, gsub, pattern = "\\(|\\)", replace = "")
+# 
+# weather_date_aggregate <- aggregate(weather_date_split, by = list(weather_data_split$Date), FUN = mean)
+# 
+# weather_date_aggregate2 <- weather_date_split %>% 
+#   group_by(Date) %>% 
+#   summarise(AirTemperature = mean(AirTemperature))
+# 
+# write.csv(weather, "data/millions.csv")
 
 # ---- CRIME DATA ----
 crime_data <- as.data.frame(read.csv("data/Crime_Data.csv"))

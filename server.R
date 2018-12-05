202888b3833a3932e007f769fcafb31c6f4e1c01
Jin Ning Huang <jinh48@uw.edu>
2018-12-05 01:22:16 -0800

    google key

--- server.R
+++ server.R
@@ -12,7 +12,12 @@ library(maps)
 library(mapdata)
 
 source("process_data.R")
+source("key.R")
 # View(crime)
+
+# response_seattle <- GET(paste0(uri_civic, "/representatives"), query = params_civic) 
+# content_seattle <- content(response_civic, "text")
+# seattle_data <- fromJSON(content_civic)
 
 # ---- SETTING UP WASHINGTON CRIME DATA ---- #
 states <- map_data("state")
@@ -20,19 +25,22 @@ washington <- subset(states, region == "washington")
 counties <- map_data("county")
 wa_county <- subset(counties, region == "washington")
 
-  washington_base <- ggplot(data = washington, mapping = aes(x = long, y = lat, group=group)) +
-   geom_polygon(fill = "palegreen", color = "black") +
-   coord_fixed(xlim = c(-122.00, -122.80), ylim = c(47.3,47.90), ratio = 1) +
-   theme_nothing() +
-   geom_polygon(data = wa_county, fill = NA, color = "white") +
-   geom_polygon(color = "black", fill = NA) +
-   geom_point(data = crime, mapping = aes(x = crime$Longitude, y = crime$Latitude),
-              color = "red", inherit.aes = FALSE)
-   #geom_tile(data = winter_rain_averages, aes(x = long, y = lat, alpha = values), fill = 'blue')
-  washington_base + geom_polygon(data = winter_rain_averages, aes(fill = values), color = "white") +
-    geom_polygon(color = "darkgreen", fill = NA) +
-    theme_void()
-   #scale_fill_gradient(low = "darkgreen", high = "lightgreen")
+map.seattle_city <- qmap("seattle", zoom = 11, source="stamen", maptype="toner",darken = c(.3,"#BBBBBB"))
+
+
+  # washington_base <- ggplot(data = washington, mapping = aes(x = long, y = lat, group=group)) +
+  #  geom_polygon(fill = "palegreen", color = "black") +
+  #  coord_fixed(xlim = c(-122.00, -122.80), ylim = c(47.3,47.90), ratio = 1) +
+  #  theme_nothing() +
+  #  geom_polygon(data = wa_county, fill = NA, color = "white") +
+  #  geom_polygon(color = "black", fill = NA) +
+  #  geom_point(data = crime, mapping = aes(x = crime$Longitude, y = crime$Latitude),
+  #             color = "red", inherit.aes = FALSE)
+  #  #geom_tile(data = winter_rain_averages, aes(x = long, y = lat, alpha = values), fill = 'blue')
+  # washington_base + geom_polygon(data = winter_rain_averages, aes(fill = values), color = "white") +
+  #   geom_polygon(color = "darkgreen", fill = NA) +
+  #   theme_void()
+  #  #scale_fill_gradient(low = "darkgreen", high = "lightgreen")
 
 
 # ---- SETTING UP PIE CHARTS FOR SEASON ---- #

a5e3e7afe5d08458a848169f53cd01836b08350f
Nikolai Liang <nikolaihegeman@gmail.com>
2018-12-05 16:10:43 -0800

    getting the 3d plot to work

--- server.R
+++ server.R
@@ -141,8 +141,8 @@ server <- function(input, output) {
     # autumn <- crime_rain[crime_rain$Occurred.Date >= "2018-09-01" & crime_rain$Occurred.Date <= "2018-11-30",]
     # winter <- crime_rain[crime_rain$Occurred.Date >= "2018-12-01" | crime_rain$Occurred.Date <= "2018-02-28",]
 
-    p <- plot_ly(crime_rain, x = ~Air.Temperature, y = ~values, z = ~x, colors = "#BF382A") %>% #text = ~date,
-      add_markers() %>%
+    p <- plot_ly(crime_rain, x = ~Air.Temperature, y = ~values, z = ~x, text = ~date, colors = "#BF382A") %>% #text = ~date,
+      # add_markers() %>%
       layout(scene = list(xaxis = list(title = 'Air Temperature'),
                           yaxis = list(title = 'Rain Percipitation'),
                           zaxis = list(title = 'Number of Crimes')))
